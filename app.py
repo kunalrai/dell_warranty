a400a10418b1e5de6e0da77cd497ee24cdc1d65e
Kunal Rai <ikunalrai@gmail.com>
2025-05-12 21:12:08 +0530

    healt check

--- app.py
+++ app.py
@@ -47,5 +47,10 @@ def index():
 def download_file(filename):
     return send_from_directory(app.config['REPORT_FOLDER'], filename, as_attachment=True)
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
+
 if __name__ == "__main__":
     app.run(debug=True)
