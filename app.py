529b3cddae196120daaa992ed7462c7abcc1a29d
Kunal Rai <hclraik@bd.com>
2025-06-04 21:43:33 +0530

    new landing page

--- app.py
+++ app.py
@@ -157,6 +157,10 @@ def health():
     return "OK", 200
 
 @app.route("/")
+def index():
+    return render_template("index.html") 
+
+@app.route("/home")
 
 def home():
     return render_template("home.html") 
